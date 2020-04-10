ad9253e06079d9a90c97a749f45c15e8363acf5c
tsuipo <tsuipo@outlook.com>
2020-04-11 01:01:16 +0800

    version 0.0.1(alpha)
    喜大普奔!可以跑的第一版,博弈树能否正常展开待测试!

--- chess.cpp
+++ chess.cpp
@@ -22,95 +22,164 @@ using namespace std;
 int currentSize;//记录每次走法生成的个数
 
 struct Point{ //点结构
-	int x,y;
+    int x,y;
 };
 struct Point_1{ //点结构
-	int x,y;
-	int score;
+    int x,y;
+    int score;
 };
 struct Step{ //步结构
-	Point first,second;
-	int value;
+    Point first,second;
+    int value;
 };
 int Board[19][19]; //存储棋盘信息，其元素值为 BLACK, WHITE, EMPTY 之一
-int evaluate(int computerSide, int simuBoard[19][19] = Board);// 第一个参数表示我方棋颜色,第二个参数为棋盘当前状态(其默认参数为全局量Board)
+int evaluate(int computerSide, int simuBoard[19][19] = Board){// 第一个参数表示我方棋颜色,第二个参数为棋盘当前状态(其默认参数为全局量Board)
+    return 1;
+}
 int mySide=BLACK; // 方便起见,增加我方
 
-
-bool hasNeighbor(int x, int y, int simuBoard[19][19] = Board){//存储合法的走法，
-	int direction_x[7] = {0, 1, 2, 3, -1, -2, -3};
-	int direction_y[7] = {0, 1, 2, 3, -1, -2, -3};
-	for(int i  = 0; i < 7; i++)
-		for(int  j = 1; j < 7; j++)
-			if (simuBoard[x + direction_x[i]][y + direction_y[j]] != 2)
-				return true;
-	for(int  i = 1; i < 7; i++)
-			if (simuBoard[x + direction_x[i]][y] != 2)
-				return true;
-	return false;
-}
-
-int getValue(int x, int y, int computerSide, int simuBoard[19][19] = Board){ // 棋盘修改为局部变量
-
-}
-
-bool sortByM1( const Step &v1, const Step &v2)//注意：本函数的参数的类型一定要与vector中元素的类型一致
-{
-    return v1.value < v2.value;//升序排列
-}
-
-// 两步的
-vector<Step>* generateMove(int computerSide, int simuBoard[19][19] = Board){
-	// std::vector<Step>::iterator it;
-	std::vector<Point_1> validCondition;
-	std::vector<Point_1>::iterator ite;
-    // for (it = moveCondition.begin(); it != moveCondition.end(); ){
-    //     it = moveCondition.erase(it);
-    // }
-	// for (it_1 = validCondition.begin(); it_1 != validCondition.end(); ){
-    //     it_1 = validCondition.erase(it_1);
-    // }
-	//寻找可以下的点
-	vector<Step>* toReturn = new vector<Step>;
-	for(int i  = 0; i < 19; i++)
-		for(int  j = 1; j < 19; j++){
-				if (simuBoard[i][j] == 2){
-					if(hasNeighbor(i, j, simuBoard)){
-						int temp = getValue(i, j, computerSide, simuBoard);
-						Point_1 temp_struct;
-						temp_struct.x = i;
-						temp_struct.y = j;
-						temp_struct.score = temp;
-						validCondition.push_back(temp_struct);
-					}
-				}
-		}
-	//将可以下的点组合，要将两步映射为一步
-	for(int i  = 0; i < validCondition.size(); i++)
-		for(int  j = i + 1; j < validCondition.size(); j++){
-			Step temp;
-			temp.first.x = validCondition[i].x;
-			temp.first.y = validCondition[i].y;
-			temp.second.x = validCondition[j].x;
-			temp.second.y = validCondition[j].y;
-			temp.value = validCondition[i].score + validCondition[j].score;
-			toReturn->push_back(temp);
-		}
-	//将toReturn里面的元素按评估分数升序排列
-	std::sort(toReturn->begin(),toReturn->end(),sortByM1);
-	return toReturn;
-}
-
-
-void copyStep(Step to, Step from){
+void copyStep(Step to, Step from){ // 对Step进行数值拷贝
     to.first.x = from.first.x;
     to.first.y = from.first.y;
     to.second.x = from.second.x;
     to.second.y = from.second.y;
 }
 
+bool isInRange(int x, int y)
+{
+    return (x>=0&&x<19&&y>=0&&y<19);
+}
+
+int placeNotEmpty(int simuBoard[19][19] = Board){
+    int cnt = 0;
+    re(i,0,19)
+        re(j,0,19){
+            if(simuBoard[i][j] != EMPTY){
+                ++cnt;
+            }
+        }
+    return cnt;
+}
+int whoWin(int side = mySide, int simuBoard[19][19] = Board){ // 返回值1:side方赢;2:side的反方赢
+    /* (0,0) (0,1) (0,2)....
+     * .
+     * .
+     * (18,0) (18,1)........(18,18)
+     * */
+    int dir[4][2] = {0, 1, 1, 0,1, 1, 1, 1 - 1};
+
+    int cnt = 0;
+    re(i, 0, 19)
+        re(j, 0, 19) { // 每点遍历
+            if (simuBoard[i][j] == mySide) { // 看我方执棋是否六连
+                re(l, 0, 4) {// 四个方向
+                    cnt = 0;
+                    re(k, 0, 6) {
+                        if (isInRange(i + k * dir[0][0], j + k * dir[0][1])) {
+                            if (simuBoard[i + k * dir[0][0]][j + k * dir[0][1]] != mySide){
+                                break;
+                            }else ++cnt; // 连子数+1
+                        } else break; // 出现界外
+                        if(cnt == 6)
+                            return 1;
+                    }
+                }
+            }
+            else if (simuBoard[i][j] == 1 - mySide) {
+                re(l, 0, 4) {// 四个方向
+                    cnt = 0;
+                    re(k, 0, 6) {
+                        if (isInRange(i + k * dir[0][0], j + k * dir[0][1])) {
+                            if (simuBoard[i + k * dir[0][0]][j + k * dir[0][1]] != 1 - mySide){
+                                break;
+                            }else ++cnt; // 连子数+1
+                        } else break; // 出现界外
+                        if(cnt == 6)
+                            return -1;
+                    }
+                }
+            }
+    }
+    return 0; // 未决出胜负
+}
+
+bool hasNeighbor(int x, int y, int simuBoard[19][19] = Board){ // 存储合法的走法
+    int direction_x[7] = {0, 1, 2, 3, -1, -2, -3};
+    int direction_y[7] = {0, 1, 2, 3, -1, -2, -3};
+    for(int i  = 0; i < 7; i++)
+        for(int  j = 1; j < 7; j++)
+            if (simuBoard[x + direction_x[i]][y + direction_y[j]] != 2)
+                return true;
+    for(int  i = 1; i < 7; i++)
+        if (simuBoard[x + direction_x[i]][y] != 2)
+            return true;
+    return false;
+}
+
+int getValue(int x, int y, int computerSide, int simuBoard[19][19] = Board){ // 棋盘修改为局部变量
+    return 1;
+}
+
+bool sortByM1(const Step &v1, const Step &v2){ //注意：本函数的参数的类型一定要与vector中元素的类型一致
+    return v1.value < v2.value;//升序排列
+}
+
+// 两步的
+vector<Step>* generateMove(int computerSide, int simuBoard[19][19] = Board){
+    // std::vector<Step>::iterator it;
+    std::vector<Point_1> validCondition;
+    std::vector<Point_1>::iterator ite;
+    // for (it = moveCondition.begin(); it != moveCondition.end(); ){
+    //     it = moveCondition.erase(it);
+    // }
+    // for (it_1 = validCondition.begin(); it_1 != validCondition.end(); ){
+    //     it_1 = validCondition.erase(it_1);
+    // }
+    //寻找可以下的点
+    vector<Step>* toReturn = new vector<Step>;
+    for(int i  = 0; i < 19; i++)
+        for(int  j = 1; j < 19; j++){
+            if (simuBoard[i][j] == 2){
+                if(hasNeighbor(i, j, simuBoard)){
+                    int temp = getValue(i, j, computerSide, simuBoard);
+                    Point_1 temp_struct;
+                    temp_struct.x = i;
+                    temp_struct.y = j;
+                    temp_struct.score = temp;
+                    validCondition.push_back(temp_struct);
+                }
+            }
+        }
+    //将可以下的点组合，要将两步映射为一步
+    for(int i  = 0; i < validCondition.size(); i++)
+        for(int  j = i + 1; j < validCondition.size(); j++){
+            Step temp;
+            temp.first.x = validCondition[i].x;
+            temp.first.y = validCondition[i].y;
+            temp.second.x = validCondition[j].x;
+            temp.second.y = validCondition[j].y;
+            temp.value = validCondition[i].score + validCondition[j].score;
+            toReturn->push_back(temp);
+        }
+    //将toReturn里面的元素按评估分数升序排列
+    std::sort(toReturn->begin(),toReturn->end(),sortByM1);
+    return toReturn;
+}
+
 int negaMax(int whosTurn, int depth, int alpha, int beta,int simuBoard[19][19]=Board){
-    // TODO:出现平局、胜负情况下的判定返回,即没有child的情况
+    // TODO: 出现平局、胜负情况下的判定返回,即没有child的情况
+    // FIXME: 确定whoWin的参数是mySide还是whosTurn
+    //平局
+    if(whoWin(whosTurn,simuBoard)==0&&placeNotEmpty(simuBoard)==19*19)
+        return 0;
+    // FIXME: 返回值取决于评估值的上限
+    else if(whoWin(whosTurn,simuBoard)==1){
+        return INF;
+    }
+    // FIXME: 返回值取决于评估值的下限
+    else if(whoWin(whosTurn,simuBoard)==-1){
+        return (-1)*INF;
+    }
     if(depth == 0){ // 叶节点
         return (whosTurn==mySide?1:-1)*evaluate(whosTurn,simuBoard);
     }
@@ -121,7 +190,7 @@ int negaMax(int whosTurn, int depth, int alpha, int beta,int simuBoard[19][19]=B
     memcpy(subBoard,simuBoard,sizeof(int[19][19]));
     vector<Step>* toMove = generateMove(whosTurn,simuBoard);
     re(i,0,(*toMove).size()){
-		// move
+        // move
         subBoard[(*toMove)[i].first.x][(*toMove)[i].first.y] = whosTurn;
         subBoard[(*toMove)[i].second.x][(*toMove)[i].second.y] = whosTurn;
         negaMaxValue = (-1)*negaMax(1-whosTurn, depth-1,(-1)*alpha,(-1)*beta,subBoard);
@@ -130,8 +199,8 @@ int negaMax(int whosTurn, int depth, int alpha, int beta,int simuBoard[19][19]=B
         if(alpha>=beta){ // 隐式剪枝
             return alpha;
         }
-		// unmove
-		subBoard[(*toMove)[i].first.x][(*toMove)[i].first.y] = EMPTY;
+        // unmove
+        subBoard[(*toMove)[i].first.x][(*toMove)[i].first.y] = EMPTY;
         subBoard[(*toMove)[i].second.x][(*toMove)[i].second.y] = EMPTY;
     }
     return highestScore;
@@ -171,112 +240,116 @@ Step aGoodStep(int depth){
 
 int main()
 {
-	Step step;//临时步结构
-	char message[256];//通信消息缓冲
+    Step step;//临时步结构
+    char message[256];//通信消息缓冲
     int computerSide;//己方执棋颜色
     int start=0;//对局开始标记
-	srand(int(time(0)));
-	//此处放置初始化代码
+    srand(int(time(0)));
+    //此处放置初始化代码
     //...
-	while (1)	//程序主循环
-	{
-		fflush(stdout);//不要删除此语句，否则程序会出问题
-		scanf("%s", message);//获取平台命令消息
+    while (1)	//程序主循环
+    {
+        fflush(stdout);//不要删除此语句，否则程序会出问题
+        scanf("%s", message);//获取平台命令消息
         //分析命令
-		if (strcmp(message, "name?") == 0)//向对战平台发送队名
-		{
-			fflush(stdin);
-			/***********将"令狐冲"改为你的队名，不超过6个汉字或12个英文字母，否则无成绩************/
-			/*******/		printf("name Eureka\n");		/**只修改令狐冲，不要删除name空格****/
-			/***********将"令狐冲"改为你的队名，不超过6个汉字或12个英文字母，否则无成绩************/
-		}
-		else if (strcmp(message, "new") == 0)//建立新棋局,下先手棋
-		{
-			int i, j;
-			scanf("%s", message);//获取己方执棋颜色
-			fflush(stdin);
-			if (strcmp(message, "black") == 0)	computerSide = BLACK;  //执黑
-			else  computerSide = WHITE;   //执白
+        if (strcmp(message, "name?") == 0)//向对战平台发送队名
+        {
+            fflush(stdin);
+            /***********将"令狐冲"改为你的队名，不超过6个汉字或12个英文字母，否则无成绩************/
+            /*******/		printf("name Eureka\n");		/**只修改令狐冲，不要删除name空格****/
+            /***********将"令狐冲"改为你的队名，不超过6个汉字或12个英文字母，否则无成绩************/
+        }
+        else if (strcmp(message, "new") == 0)//建立新棋局,下先手棋
+        {
+            int i, j;
+            scanf("%s", message);//获取己方执棋颜色
+            fflush(stdin);
+            if (strcmp(message, "black") == 0)	computerSide = BLACK;  //执黑
+            else  computerSide = WHITE;   //执白
 
-			for (i = 0; i<19; ++i)   //初始化棋局
-				for (j = 0; j<19; ++j)
-					Board[i][j] = EMPTY;
-			start = 1;
+            for (i = 0; i<19; ++i)   //初始化棋局
+                for (j = 0; j<19; ++j)
+                    Board[i][j] = EMPTY;
+            start = 1;
 
-			if (computerSide == BLACK)
-			{
-				/**********生成第一手着法，并保存在step结构中，落子坐标为(step.first.x,step.first.y)**********/
-				/****************************在下方填充代码，并替换我的示例代码******************************/
+            if (computerSide == BLACK)
+            {
+                /**********生成第一手着法，并保存在step结构中，落子坐标为(step.first.x,step.first.y)**********/
+                /****************************在下方填充代码，并替换我的示例代码******************************/
 
-				// 第一步落子其实没啥好换的,直接手动优化
-				step.first.x = 9;
-				step.first.y = 9;
+                // 第一步落子其实没啥好换的,直接手动优化
+                step.first.x = 9;
+                step.first.y = 9;
 
 
-				/******************************在上面填充第一步行棋代码*******************************************/
+                /******************************在上面填充第一步行棋代码*******************************************/
 
-				Board[step.first.x][step.first.y] = computerSide;//处理己方行棋
-				printf("move %c%c@@\n", step.first.x + 'A', step.first.y + 'A');//输出着法
-			}
-		}
-		else if(strcmp(message,"move")==0)//行棋,本程序核心,正常下棋
-		{
-			scanf("%s", message);//获取对手行棋着法
-            fflush(stdin); 
-			step.first.x=message[0]-'A';		step.first.y=message[1]-'A';
-			step.second.x=message[2]-'A';		step.second.y=message[3]-'A';
+                Board[step.first.x][step.first.y] = computerSide;//处理己方行棋
+                printf("move %c%c@@\n", step.first.x + 'A', step.first.y + 'A');//输出着法
+            }
+        }
+        else if(strcmp(message,"move")==0)//行棋,本程序核心,正常下棋
+        {
+            scanf("%s", message);//获取对手行棋着法
+            fflush(stdin);
+            step.first.x=message[0]-'A';		step.first.y=message[1]-'A';
+            step.second.x=message[2]-'A';		step.second.y=message[3]-'A';
             //处理对手行棋
-			Board[step.first.x][step.first.y] = 1 - computerSide;
-			if(!(step.second.x==-1 && step.second.y==-1)) Board[step.second.x][step.second.y] = 1 - computerSide;
+            Board[step.first.x][step.first.y] = 1 - computerSide;
+            if(!(step.second.x==-1 && step.second.y==-1)) Board[step.second.x][step.second.y] = 1 - computerSide;
 
             /**********************************************************************************************************/
-			/***生成落子的坐标，保存在step结构中，第1子下在(step.first.x,step.first.y)，第2子下在(step.first.x,step.first.y)*****/
-			/**************************************在下方填充代码，并替换我的示例代码*****************************************/
-			
-			mySide = computerSide;
-			//生成第1子落子位置step.first.x和step.first.y
-			int x, y;
-			x = rand() % 19; y = rand() % 19;
-			while (Board[x][y] != EMPTY)
-			{
-				x = rand() % 19;	y = rand() % 19;
-			}
-			step.first.x = x;
-			step.first.y = y;
-			Board[step.first.x][step.first.y] = computerSide;
+            /***生成落子的坐标，保存在step结构中，第1子下在(step.first.x,step.first.y)，第2子下在(step.first.x,step.first.y)*****/
+            /**************************************在下方填充代码，并替换我的示例代码*****************************************/
 
-			//生成第2子落子位置step.second.x和step.second.y	
-			x = rand() % 19;	y = rand() % 19;
-			while (Board[x][y] != EMPTY)
-			{
-				x = rand() % 19;	y = rand() % 19;
-			}
-			step.second.x = x;
-			step.second.y = y;
-			Board[step.second.x][step.second.y] = computerSide;
-			
-			/*****************************************在上面填充代码******************************************************/
-			/**********************************************************************************************************/
+            mySide = computerSide;
+            // TODO: 决定搜索深度
+            int depth = 3;
+            Step toMove = aGoodStep(depth);
+            Board[toMove.first.x][toMove.first.y] = computerSide;
+            Board[toMove.second.x][toMove.second.y] = computerSide;
+//            //生成第1子落子位置step.first.x和step.first.y
+//            int x, y;
+//            x = rand() % 19; y = rand() % 19;
+//            while (Board[x][y] != EMPTY)
+//            {
+//                x = rand() % 19;	y = rand() % 19;
+//            }
+//            step.first.x = x;
+//            step.first.y = y;
+//            Board[step.first.x][step.first.y] = computerSide;
+//
+//            //生成第2子落子位置step.second.x和step.second.y
+//            x = rand() % 19;	y = rand() % 19;
+//            while (Board[x][y] != EMPTY)
+//            {
+//                x = rand() % 19;	y = rand() % 19;
+//            }
+//            step.second.x = x;
+//            step.second.y = y;
+//            Board[step.second.x][step.second.y] = computerSide;
 
-			printf("move %c%c%c%c\n",step.first.x+'A',step.first.y+'A',step.second.x+'A',step.second.y+'A');//输出着法
-		}
+            /*****************************************在上面填充代码******************************************************/
+            /**********************************************************************************************************/
+
+            printf("move %c%c%c%c\n",step.first.x+'A',step.first.y+'A',step.second.x+'A',step.second.y+'A');//输出着法
+        }
         else if (strcmp(message, "error") == 0)//着法错误
         {
             fflush(stdin);
         }
-		else if (strcmp(message, "end") == 0)//对局结束
-		{
+        else if (strcmp(message, "end") == 0)//对局结束
+        {
             fflush(stdin);
-			start = 0;
-		}
-		else if (strcmp(message, "quit") == 0)//退出引擎
-		{
+            start = 0;
+        }
+        else if (strcmp(message, "quit") == 0)//退出引擎
+        {
             fflush(stdin);
-			printf("Quit!\n");
-			break;
-		}
-	}
-	return 0;
+            printf("Quit!\n");
+            break;
+        }
+    }
+    return 0;
 }
 
- 
